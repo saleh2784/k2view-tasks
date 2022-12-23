620f13c1804707b009d5d24facc8e1f834061aaf
saleh2784 <60964062+saleh2784@users.noreply.github.com>
2022-12-23 02:17:56 +0200

    updated scripts

--- installations/1-node/1-cassandra_node.sh
+++ installations/1-node/1-cassandra_node.sh
@@ -5,12 +5,11 @@ cd ..
 
 echo "Download cassandra version  3.11.9"
 
-wget --no-check-certificate https://download.k2view.com/index.php/s/n7H7bZs2HMKKZF7/download
+# wget --no-check-certificate https://download.k2view.com/index.php/s/n7H7bZs2HMKKZF7/download
 
 tar -zxvf download
-
-sed -i '11i\alias python='/usr/bin/python2.7'\' ~/.bash_profile source ./.bash_profile
-# verified the Python version 
+sed -i '11i\alias python='/usr/bin/python2.7'\' ~/.bash_profile
+source ./.bash_profile
 python --version
 bash -l
 
@@ -18,7 +17,9 @@ bash -l
 #dc=DC1
 #cluster_name = cassandra
 ######################################################################################
-sleep 5
+echo "run seeds >>>>>>>>>>>>>>>>>>>>>>>>>"
+
+sleep 30
 
 sed -i "s@INSLATT_DIR=.*@INSLATT_DIR=$(pwd)@" .bash_profile
 rm -rf  cassandra/data && ln -s /opt/apps/cassandra/storage/  cassandra/data
@@ -28,38 +29,34 @@ sed -i s/seeds:.*/"seeds: $(hostname -I |awk {'print $1'})"/g $INSLATT_DIR/cassa
 sed -i s/listen_address:.*/"listen_address: $(hostname -I |awk {'print $1'})"/g $INSLATT_DIR/cassandra/conf/cassandra.yaml
 sed -i s/broadcast_rpc_address:.*/"broadcast_rpc_address: $(hostname -I |awk {'print $1'})"/g $INSLATT_DIR/cassandra/conf/cassandra.yaml
 sed -i 's@endpoint_snitch:.*@endpoint_snitch: GossipingPropertyFileSnitch@' $INSLATT_DIR/cassandra/conf/cassandra.yaml
-sed -i 's@LOCAL_JMX=.*@LOCAL_JMX='n'@' $INSLATT_DIR/cassandra/conf/cassandra-env.sh
+sed -i 's@LOCAL_JMX=.*@LOCAL_JMX='no'@' $INSLATT_DIR/cassandra/conf/cassandra-env.sh
 sed -i "s@-Djava.rmi.server.hostname=.*@-Djava.rmi.server.hostname=$(hostname -I |awk {'print $1'})\"@" $INSLATT_DIR/cassandra/conf/cassandra-env.sh
 sed -i "s@-Dcom.sun.management.jmxremote.password.file=.*@-Dcom.sun.management.jmxremote.password.file=/opt/apps/cassandra/cassandra/conf/.jmxremote.password\"@" $INSLATT_DIR/cassandra/conf/cassandra-env.sh
-{
-    echo "monitorRole QED"
-    echo "controlRole R&D"
-    echo "cassandra cassandra"
-    echo "k2view Q1w2e3r4t5"
-
-} >> $INSLATT_DIR/cassandra/conf/.jmxremote.password
+chmod 777 $INSLATT_DIR/cassandra/conf/.jmxremote.password
+echo "monitorRole QED" >> $INSLATT_DIR/cassandra/conf/.jmxremote.password > /dev/null
+echo "controlRole R&D" >> $INSLATT_DIR/cassandra/conf/.jmxremote.password > /dev/null
+echo "cassandra cassandra" >> $INSLATT_DIR/cassandra/conf/.jmxremote.password > /dev/null
+echo "k2view Q1w2e3r4t5" >> $INSLATT_DIR/cassandra/conf/.jmxremote.password > /dev/null
 chmod 400 ~/cassandra/conf/.jmxremote.password
-
-# echo "monitorRole QED" >> ~/cassandra/conf/.jmxremote.password
-# echo "controlRole R&D" >> ~/cassandra/conf/.jmxremote.password
-# echo "cassandra cassandra" >> ~/cassandra/conf/.jmxremote.password
-# echo "k2view Q1w2e3r4t5" >> ~/cassandra/conf/.jmxremote.password
-
-sleep 5
 ######################################################################################
-echo "start cassandra service"
+echo ""
+echo " start cassandra >>>>>>>>>>>>"
 
-cassandra 
+cassandra > /dev/null 
 
-
+sleep 30
 ######################################################################################
 
-# echo "create user k2admin"
-
-# echo "create user k2admin with password 'Q1w2e3r4t5' superuser;" |cqlsh -u cassandra -p cassandra
-
-#######################################################################################
+echo ""
+echo "creating user k2admin >>>>>>>>>>>>"
+echo ""
+echo "create user k2admin with password 'Q1w2e3r4t5' superuser;" |cqlsh -u cassandra -p cassandra
+######################################################################################
 
 #Check cassandra's status 
 
+echo ""
+echo " check the status node >>>>>>>>>>>>"
+
 nodetool -u cassandra -pw cassandra status
+
