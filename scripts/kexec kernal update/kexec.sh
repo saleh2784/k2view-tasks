bca721d9860fd851c32158f08b2f1359a80c6329
saleh2784 <60964062+saleh2784@users.noreply.github.com>
2023-01-10 10:52:37 +0200

    update kexec script

--- scripts/kexec kernal update/kexec.sh
+++ scripts/kexec kernal update/kexec.sh
@@ -1,17 +1,16 @@
 #!/bin/bash 
 
-KERNEL_VERSION=$(rpm -qa --last | grep 'kernel-[0-9]' | sed -n 1p | sed "s/^.*kernel-//" | sed "s/ .*//")
-
 echo "installing kexec & updating the kernel ..........."
 sudo yum install kexec-tools && sudo yum update kernel -y
 
-echo "booting vmlinuz .........."
-sudo kexec -l /boot/vmlinuz-$KERNEL_VERSION --initrd=/boot/initramfs-$KERNEL_VERSION.img --reuse-cmdline
-#sudo kexec -u && kexec -l /boot/vmlinuz-$KERNEL_VERSION --initrd=/boot/initramfs-$KERNEL_VERSION.img --reuse-cmdline
+KERNEL_VERSION=$(rpm -qa --last | grep 'kernel-[0-9]' | sed -n 1p | sed "s/^.*kernel-//" | sed "s/ .*//")
 
-sleep 10
+echo "booting vmlinuz .........."
+kexec -u && kexec -l /boot/vmlinuz-$KERNEL_VERSION --initrd=/boot/initramfs-$KERNEL_VERSION.img --reuse-cmdline
+
+sleep 3
 
 echo "kexec reloading ......."
-sudo kexec -u && sudo systemctl kexec
+systemctl kexec
 sleep 120
 
